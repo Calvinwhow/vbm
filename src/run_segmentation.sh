84f584aa4beb240b0a066d37d00f6c2ba9c3393a
Calvinwhow <calvin.howard@ucalgary.ca>
2024-11-07 23:45:36 -0500

    Code updates

--- src/run_segmentation.sh
+++ src/run_segmentation.sh
@@ -6,7 +6,7 @@ CAT12_DIR=$CAT_PATH
 MATLAB_RUNTIME=$MCRROOT
 
 # 1) Run the CAT12 standalone segmentation script
-cat_standalone.sh -b /opt/spm/standalone/cat_standalone_segment_calvin.m \
+cat_standalone.sh -b /scripts/cat_standalone_segment_calvin.m \
                     /data/*.nii*
 
 # Wait for the segmentation to produce the output files
@@ -16,9 +16,9 @@ while [ $(ls /data/mri/*mwp*.nii 2> /dev/null | wc -l) -eq 0 ]; do
 done
 
 # 2) Run the CAT12 Standalone Smoothing 
-cat_standalone.sh -b /opt/spm/standalone/cat_standalone_smooth_calvin.m \
+cat_standalone.sh -b /scripts/cat_standalone_smooth_calvin.m \
                     /data/mri/*mwp*.nii
 
 # 3) Extract TIV for each file. 
-cat_standalone.sh -b /opt/spm/standalone/cat_standalone_get_TIV_calvin.m \
+cat_standalone.sh -b /scripts/cat_standalone_get_TIV_calvin.m \
                     /data/report/cat_*.xml 
